a996e8c1e380426a6fefdd90ee57da0956d461b2
Donna Torres <todonnatorres@icloud.com>
2020-06-02 02:11:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence)
-  array = sentence.split
+def reverse_each_word(string)
+  array = sentence.split(" ")
   sentence.collect do |sentence|
   sentence.split(',').reverse
 end
